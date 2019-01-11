b70fb36851c8f9297368715d6737cf095504c39b
khalil <khalil.pan2@gmail.com>
2019-01-11 15:15:57 -0300

    adding course class with annotations (@Many To One)

---
+++ src/com/khalilpan/entity/course.java
@@ -0,0 +1,69 @@
+package com.khalilpan.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="course")
+public class course {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@ManyToOne(cascade= {CascadeType.PERSIST,CascadeType.DETACH
+						,CascadeType.MERGE,CascadeType.REFRESH}) //to prevent to delete he user when deleting one course
+	@JoinColumn(name="userid") // "userid" is the name of field in table that is connected to user table 
+	private User user;
+
+	public course() {
+		}
+
+	public course(String title) {
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "course [id=" + id + ", title=" + title + ", user=" + user + "]";
+	}
+	
+	
+	
+
+}
